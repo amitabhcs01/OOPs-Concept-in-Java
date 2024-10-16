ec1139531314c986a318691ab7edc3030df77a9b
Amitabh Ranjan <amitabh1ranjan@gmail.com>
2024-10-17 01:58:20 +0530

    Encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Capsule{
+    public int num1; //data members 
+    public int num2;//data members 
+
+    public int getSum(int num1 ,  int num2) //memberFunction (Methods)
+    {
+        return num1 + num2;
+    }
+}
+public class Encapsulation {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter num1: ");
+        int num1  = sc.nextInt();
+
+        System.out.print("Enter num2: ");
+        int num2 = sc.nextInt();
+
+        Capsule obj = new Capsule();
+
+        int result = obj.getSum(num1 , num2 );
+        System.out.println("Your sum is : " + result);
+    }
+
+    
+}
