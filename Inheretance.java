16288be564a840d96764ba85c236a73616c25328
Amitabh Ranjan <amitabh1ranjan@gmail.com>
2024-10-18 01:34:06 +0530

    Defination added

---
+++ Inheretance.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+//Inheretance :  it is a concept in OOPs that deals with sharing property of Parent Class to child class
+//Inheretance is Just Like "Nepotism" like Parent give their fame to their child;
+/*
+ * With the help of Inheretance Child class can acces property of their parents class
+ * Inheretance is helpfull for code reusabilty
+ * Component involved in Inheretance: 
+ * 1} General Terms to remember: 
+ *     i) parents class / base class / super class;
+ *     ii) Child class / derived class
+ * 2} we can inherit property of the parent class by using "extend" keyword in child class ;
+ * 
+ * Types of Inheretance: 
+ * 1> Single Inheretance
+ * 2> Multi-level Inheretance
+ * 3> Multiple Inheretance #not supported in java but in other languages like c++ multiple inhertance is there
+ * 
+ * In this whole process of Learning Inheretance we will built our own Calculator;
+ * So, lets Start....
+ * 1> Single Inheretance :
+ *  => one parent class and one base class is there
+ *   for example: you have some property like your father may be you have eyes like your father 
+ *                so , in single inheretance one parent class inherit its property to the base class;
+ * **Syntex:
+ * 
+ * class Father{
+ *   //statement(mathods)
+ * }
+ * class Child extend Father{
+ *   //statement
+ * }
+ * 
+ * 2> Multilevel Inheretance : 
+ *  => it have two class Parents  ex. i> Grandfather ii> Father
+ *     and also it have one base class ex. its you
+ * ex: your GrandFather's property like your grandfather is good at CSE and also your father is good at CSE , because GrandFATHER inherit his property in your father
+ *     like wise ,  you are also good at CS it means your father have inherit his property to you that he got from your Grandfather;
+ * **Syntex: 
+ *class GrandFather{
+ *   //statement(mathods)
+ * }
+ * class Father extend GrandFather{
+ *   //statement
+ * }
+ * class Child extend Father{
+ * //statement
+ * .....in this child class is indirectly use the porpert of the GrandFather;
+ * }
+ */
+
+class InhrOld{
+    
+}
+class InhrNew{
+
+}
+public class Inheretance {
+
+    
+}
