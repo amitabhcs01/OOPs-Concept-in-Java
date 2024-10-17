79e7f1245764e6109449a2f6e671c3ec02f9e161
Amitabh Ranjan <amitabh1ranjan@gmail.com>
2024-10-17 23:31:49 +0530

    parametrized constructor added

--- ConstructorInJava.java
+++ ConstructorInJava.java
@@ -46,8 +46,39 @@ class Constructor{
         }
         }
       */
+      public Constructor(String name, int age , String accountNumber, String password , String accountBalance) {
+        this.name = name;
+        this.age = age;
+        this.accountNumber = accountNumber;
+        this.password = password;
+        this.accountBalance = accountBalance;
+      }
+      public void getInfo() {
+        System.out.println("\n"+name);
+        System.out.println(age);
+        System.out.println(accountNumber);
+        System.out.println(password);
+        System.out.println(accountBalance);
+      }
 }
 public class ConstructorInJava {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter your name: ");
+      String name = sc.nextLine();
+      System.out.println("Enter your age");
+      int age =  sc.nextInt();
+      sc.nextLine();
+      System.out.println("Enter your Account number : ");
+      String accountNumber =  sc.nextLine();
+      System.out.println("Enter your password: ");
+      String password =  sc.nextLine();
+      System.out.println("Enter your Account Balance: ");
+      String accountBalance = sc.nextLine();
+
+      Constructor obj  = new Constructor(name , age, accountNumber, password, accountBalance); //if we pass parameter here and also on the class it will be parametrized constructor
+      obj.getInfo();
+    }
 
     
 }
